e9845f6b6234fe6d00b4424309a5558378ebb0b1
Kevin Welsh <kevinwelsh132@gmail.com>
2023-11-21 17:03:03 -0700

    clean it up a bit for the issue post.

--- .conda/build.sh
+++ .conda/build.sh
@@ -1,6 +1,3 @@
-export LDFLAGS="-Wl,-pie -Wl,-headerpad_max_install_names -Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
-export LDFLAGS_LD="-pie -headerpad_max_install_names -rpath $PREFIX/lib -L$PREFIX/lib"
-export CXXFLAGS="-ftree-vectorize -fPIC -fPIE -fstack-protector-strong -O2 -pipe -stdlib=libc++ -fmessage-length=0 -isystem $PREFIX/include -fdebug-prefix-map=$SRC_DIR=/usr/local/src/conda/test-osx-arm64-1 -fdebug-prefix-map=$PREFIX=/usr/local/src/conda-prefix"
 mkdir build
 cd build
 cmake .. $CMAKE_ARGS -DHeffte_ENABLE_FFTW=ON -DBUILD_SHARED_LIBS=ON
